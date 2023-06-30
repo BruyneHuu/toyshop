69b4c45af6259701f7c668781ef2ff59f7709e03
BruyneHuu <buinghialk180303@gmail.com>
2023-06-30 11:49:46 +0700

    first

--- server.js
+++ server.js
@@ -9,47 +9,30 @@ const MongoClient = require('mongodb').MongoClient
 
 const connectionString = 'mongodb+srv://buinghialk180303:1232133312a@cluster0.gg4bh7v.mongodb.net/'
 
-// (0) CONNECT: server -> connect -> MongoDB Atlas 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
-        
-        // (1a) CREATE: client -> create -> database -> 'vn-toys'
-        // -> create -> collection -> 'toys'
+  
         const db = client.db('toyshopdb')
         const quotesCollection = db.collection('product')
         
-        // To tell Express to EJS as the template engine
         app.set('view engine', 'ejs') 
         
-        // Make sure you place body-parser before your CRUD handlers!
         app.use(bodyParser.urlencoded({ extended: true }))
 
-        // To make the 'public' folder accessible to the public
         app.use(express.static('public'))
 
-        // To teach the server to read JSON data 
         app.use(bodyParser.json())
 
-        // (2) READ: client -> browser -> url 
-        // -> server -> '/' -> collection -> 'quotes' -> find() 
-        // -> results -> index.ejs -> client
         app.get('/', (req, res) => {
             db.collection('product').find().toArray()
                 .then(results => {
-
-                    // results -> server -> console
                     console.log(results)
-                    
-                    // results -> index.ejs -> client -> browser 
-                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
                     res.render('index.ejs', { quotes: results })
                 })
-                .catch(/* ... */)
+                .catch()
         })
 
-        // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
-        // -> post -> '/quotes' -> collection -> insert -> result
         app.post('/product', (req, res) => {
             quotesCollection.insertOne(req.body)
             .then(result => {
